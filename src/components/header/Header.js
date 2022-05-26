244a63c9d9db9b885145354b8e111e041e8df6c7
Gustavo <gar.santeliz@gmail.com>
2022-05-26 18:55:19 -0400

    adding search bar in header compoenent

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-// import { FaUserAlt } from 'react-icons/fa'
+import { Form, Button } from 'react-bootstrap'
+import { FaSearch } from 'react-icons/fa'
 import './Header.scss'
 
 
 export const Header = () => {
+
+  const [searchBar, setSearchBar] = useState('')
+
+  const handleInputSearch = (e) =>{
+    setSearchBar(e.target.value)
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(e)
+  }
   return (
     <div className='header'>
-      <Link to="/">
-        <div className='logo'>Movie App</div>
-      </Link>
+      
+        <div className='logo'><Link to="/">Movie App</Link></div>
+      
+    <div className='search-bar'>
+      <form>
+          <input placeholder='Search Movies or Shows' type="text" onChange={handleInputSearch} name="search" value={ searchBar } />
+          <button type='submit'><FaSearch className='FaSearch'/></button>
+      </form>
+    </div>
 
       <div className='user-image'>
-        <img src='https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png' 
-        alt='user' />
-
+        <img src='https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png'
+          alt='user' />
       </div>
 
     </div>
